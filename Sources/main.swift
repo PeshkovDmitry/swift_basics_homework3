96f4f5c911ab78fe334a9884488d29b287851507
PeshkovDmitry <peshkov-dv@yandex.ru>
2025-02-25 22:04:32 +0300

    Выполнено задание 1

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,9 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// Задание 1
 
-print("Hello, world!")
+let teas: [String: Float] = ["Assam Tea": 100, "Darjeeling" : 150, "Ceylon": 200, "Keemun": 250]
+let queue: Array<String> = ["Ceylon", "Assam Tea", "Darjeeling", "Keemun"]
+for (num, client) in queue.enumerated() {
+    if let price = teas[client] {
+        print(String(num) + ": " + client + "->" + String(price))
+    }
+}
