29cc574fc1736cccf8ff2e8872423e08fea2d781
PeshkovDmitry <peshkov-dv@yandex.ru>
2025-02-25 22:22:21 +0300

    Добавил сортировку по возрастанию

--- Sources/main.swift
+++ Sources/main.swift
@@ -11,5 +11,8 @@ for (num, client) in queue.enumerated() {
 // Задание 2
 
 let arr: [Int?] = [-4, 5, 10, nil, 4, nil, 25, 0, nil, 16, 75, -20, -7, 15, 0, nil]
-var res: [Int] = arr.filter{$0 != nil}.map{$0!}.filter{$0 != 0}.filter{$0 != 4}
+var res: [Int] = arr.filter{$0 != nil}.map{$0!}.filter{$0 != 0}.filter{$0 != 4}.sorted{$0 < $1}
 print(res)
+
+// Задание 3
+
